2431de953b696b0668eac47e0aec4f647fc49032
Alex Leschenko <a_leshenko@tut.by>
2016-07-04 23:51:54 +0300

    Project finished 04.07.2016

--- app/views.py
+++ app/views.py
@@ -22,17 +22,20 @@ def shortener(request):
                 for i in range(6):
                     url += choice('1234567890abcdef')
             Url.objects.create(url=data['url'], cr_time=time.format('YYYY - MM - DD HH:mm:ss'), short_url=url)
-            return HttpResponse("You've been create short URL")
+            data = Url.objects.filter().last()
+            context = {'data':data}
+            return render(request, 'done.html', context)
         context = {'my_form': form}
         return render(request, 'my_form.html', context)
     elif request.method == 'GET':
-        shr = request.GET.get('shr')
-        data = Url.objects.filter(short_url=shr).get()
-        Url.objects.filter(short_url=shr).update(clicks = data.clicks+1)
-        return redirect(data.url)
-    else:
-        context = {'my_form': URL_create()}
-        return render(request, 'my_form.html', context)
+        if request.GET.items():
+            shr = request.GET.get('shr')
+            data = Url.objects.filter(short_url=shr).get()
+            Url.objects.filter(short_url=shr).update(clicks=data.clicks + 1)
+            return redirect(data.url)
+        else:
+            context = {'my_form': URL_create()}
+            return render(request, 'my_form.html', context)
 
 
 def url_list(request):
