753bd59d2d383437a22d721d2e5036abe1b5eda4
Alex Leschenko <a_leshenko@tut.by>
2016-07-09 17:35:29 +0300

    done

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Url(models.Model):
     url = models.URLField(null=True)
-    cr_time = models.DateTimeField(null=True)
-    short_url = models.URLField(null=True, max_length=100)
+    cr_time = models.DateTimeField(null=True, auto_now_add=True)
+    short_url = models.URLField(null=True, max_length=100, unique=True)
     clicks = models.IntegerField(null=True, default=0)
 
