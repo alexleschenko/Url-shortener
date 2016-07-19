7eb09eed55a43476b0babb8b99f9fa6ff6486cd5
Alex Leschenko <a_leshenko@tut.by>
2016-07-19 09:52:23 +0300

    done + comments

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from random import choice
 
 from django.shortcuts import render, redirect
@@ -14,7 +15,7 @@ def shortener(request):
             data = form.cleaned_data
             url = ""
             url_list = Url.objects.filter().values('short_url')
-            while url == "" or url in url_list:
+            while url == "" or url in url_list: #генерация номера
                 url = "http://127.0.0.1:8000/?shr="
                 for i in range(6):
                     url += choice('1234567890abcdef')
@@ -26,7 +27,7 @@ def shortener(request):
         return render(request, 'my_form.html', context)
     elif request.method == 'GET':
         if request.GET.items():
-            shr = request.GET.get('shr')
+            shr = request.GET.get('shr') #счетчик кликов и переход по URL
             data = Url.objects.filter(short_url__icontains=shr).get()
             Url.objects.filter(short_url__icontains=shr).update(clicks=data.clicks + 1)
             return redirect(data.url)
